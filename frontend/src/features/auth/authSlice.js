d4c7f1ea80e8de1ec1363e399d0b144c4a6ed73a
pdxchambers <pdxchambers@gmail.com>
2023-05-30 22:38:57 -0700

    get logged in user from local storage if available

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,8 +1,10 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
 import authService from './authService'
 
+const user = JSON.parse(localStorage.getItem('user'))
+
 const initialState = {
-    user: null,
+    user: user ? user : null,
     isError: false,
     isSuccess: false,
     isLoading: false,
