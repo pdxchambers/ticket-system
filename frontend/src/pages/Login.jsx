b07b4f3a5a4e85cba460fd80bdf097499b812650
pdxchambers <pdxchambers@gmail.com>
2023-06-01 02:01:42 -0700

    add user login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,9 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import {toast} from 'react-toastify'
 import {FaSignInAlt} from 'react-icons/fa'
 import {useSelector, useDispatch} from 'react-redux'
-import {login} from '../features/auth/authSlice'
+import {login, reset} from '../features/auth/authSlice'
+import { useNavigate } from 'react-router-dom'
 
 function Login() {
     const [formData, setFormData] = useState({
@@ -13,8 +14,21 @@ function Login() {
     const {email, password} = formData
 
     const dispatch = useDispatch()
+    const navigate = useNavigate()
 
-    const {user, isLoading, isSuccess, message} = useSelector(state => state.auth)
+    const {user, isLoading, isError, isSuccess, message} = useSelector(state => state.auth)
+
+    useEffect(() => {
+        if(isError) {
+            toast.error(message)
+        }
+
+        if(isSuccess)  {
+            navigate('/')
+        }
+
+        dispatch(reset())
+    }, [isError, isSuccess, user, message, navigate, dispatch])
 
     const onChange = (e) => {
         setFormData((prevState) => ({
