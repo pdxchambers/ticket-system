104068bfbaf88f8287bbdd03d60a6dfb1b392eb5
pdxchambers <pdxchambers@gmail.com>
2023-06-14 08:14:47 -0700

    add single ticket item and ticket list

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -5,6 +5,6 @@ import ticketReducer from '../features/tickets/TicketSlice'
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    tickets: ticketReducer
+    ticket: ticketReducer
   },
 });
--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -4,9 +4,10 @@ import {useSelector, useDispatch} from 'react-redux'
 import {getTickets, reset} from '../features/tickets/TicketSlice'
 import Spinner from '../components/Spinner'
 import BackButton from '../components/BackButton'
+import TicketItem from '../components/TicketItem'
 
 function Tickets() {
-    const {tickets, isLoading, isSuccess} = useSelector((state) => state.tickets)
+    const {tickets, isLoading, isSuccess} = useSelector((state) => state.ticket)
     const dispatch = useDispatch()
 
     useEffect(() => {
@@ -26,7 +27,22 @@ function Tickets() {
     }
 
   return (
-    <h1>Tickets</h1>
+    <>
+        <BackButton url='/' />
+        <h1>Tickets</h1>
+        <div className="tickets">
+            <div className="ticket-headings">
+                <div>Date</div>
+                <div>Product</div>
+                <div>Status</div>
+                <div></div>
+            </div>
+            {tickets.map((ticket) => (
+                <TicketItem key={ticket._id} ticket = {ticket} />
+            ))}
+        </div>
+
+    </>
   )
 }
 
