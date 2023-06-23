595e57a763c6ae4c225e1a34240b3c95088a97d1
pdxchambers <pdxchambers@gmail.com>
2023-06-23 08:00:27 -0700

    add heroku routes and config

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const connectDB = require('./config/db')
@@ -13,12 +14,19 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'Welcome to the Support Desk API'})
-} )
-
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
+
+//serve front end
+if(process.env.NODE_ENV === 'production') {
+    //set build folder as static
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+} else {
+    app.get('/', (req, res) => {
+        res.status(200).json({message: 'Welcome to the Support Desk API'})
+    } )
+}
 
 app.use(errorHandler)
 
